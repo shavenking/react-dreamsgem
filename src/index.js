59b7368c67b2a775851a50be015deaf637cbfd0f
shavenking <shavenking@gmail.com>
2018-06-21 10:03:13 +0800

    Use JavaScript standard style

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react'
+import ReactDOM from 'react-dom'
 import IndexCSS from './index.css'
 
 const Index = () => {
-    return <div className={IndexCSS["btn-primary"]}>Hello React!</div>;
-};
+  return <div className={IndexCSS['btn-primary']}>Hello React!</div>
+}
 
-ReactDOM.render(<Index />, document.getElementById("index"))
+ReactDOM.render(<Index/>, document.getElementById('index'))
